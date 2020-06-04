4fedd1646f1684566dafb03c67832c8cd5a5d4d8
Justin Frahm <justinmfrahm@gmail.com>
2020-06-03 23:38:19 -0600

    Some minor keymap tweaks

--- qmk/sixty_percent/keymaps/default/keymap.c
+++ qmk/sixty_percent/keymaps/default/keymap.c
@@ -10,17 +10,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * | CLCK  |   A   |   S   |   D   |   F   |   G   | |   H   |   J   |   K   |   L   |   :   |   '   |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * | SHIFT |   Z   |   X   |   C   |   V   |   B   | |   N   |   M   |   ,   |   .   |   /   |  ENT  |
+ * | SHIFT |   Z   |   X   |   C   |   V   |   B   | |   N   |   M   |   ,   |   .   |   /   | SHIFT |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * | CTRL  |  ALT  |  WIN  | LAYER |  ESC  |  SPC  | |  SPC  | SHIFT |  HOME |  END  |  ALT  |  CTRL |
+ * | CTRL  |  ALT  |  WIN  | LAYER |  ESC  |  SPC  | |  SPC  | ENTER |  HOME |  END  |  ALT  |  CTRL |
  * `-----------------------------------------------' `-----------------------------------------------'
  */
 [0] = LAYOUT( \
   KC_GRV,   KC_1,    KC_2,      KC_3,  KC_4,   KC_5,     KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPACE, \
   KC_TAB,   KC_Q,    KC_W,      KC_E,  KC_R,   KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSLS, \
   KC_CLCK,  KC_A,    KC_S,      KC_D,  KC_F,   KC_G,     KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,      KC_C,  KC_V,   KC_B,     KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH, KC_ENT, \
-  KC_LCTRL, KC_LALT, KC_LGUI,   MO(1), KC_ESC, KC_SPC,   KC_SPC,  KC_RSFT, KC_HOME,  KC_END,  KC_RALT, KC_RCTL \
+  KC_LSFT,  KC_Z,    KC_X,      KC_C,  KC_V,   KC_B,     KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLSH, KC_RSFT, \
+  KC_LCTRL, KC_LALT, KC_LGUI,   MO(1), KC_ESC, KC_SPC,   KC_SPC,  KC_ENT, KC_HOME,  KC_END,  KC_RALT, KC_RCTL \
 ),
 
 /* LAYER 1
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |       | |  LEFT | DOWN  |  UP   | RIGHT |       |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       | |  -/_  |  =/+  |  [/{  |  ]/}  |       |       |
+ * | SHIFT |       |       |       |       |       | |  -/_  |  =/+  |  [/{  |  ]/}  |       | SHIFT |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * |       |       |       | ----- |       |       | |       |       |       |       |       |       |
  * `-----------------------------------------------' `-----------------------------------------------'
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12, \
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, \
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,  KC_NO, \
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_NO,  KC_NO, \
+  KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_NO,  KC_RSFT, \
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO \
 )
 
