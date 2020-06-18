a5929d8b96ffdaa8b8bed6792d5b86fd8dfda257
Justin Frahm <justinmfrahm@gmail.com>
2020-06-18 11:03:12 -0600

    Shift a keymap row

--- firmware/lap_keys/v0.2/keymaps/default/keymap.c
+++ firmware/lap_keys/v0.2/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * |       |       |       |       |       |       | |  LEFT | DOWN  |  UP   | RIGHT |       |       |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
- * | ----- |       |       |       |       |       | |  -/_  |  =/+  |  [/{  |  ]/}  |       | ----- |
+ * | ----- |       |       |       |       |       | |       |  -/_  |  =/+  |  [/{  |  ]/}  | ----- |
  * |-------+-------+-------+-------+-------+-------| |-------+-------+-------+-------+-------+-------|
  * | ----- | ----- | ----- | ----- |       |       | |       |       | ----- | ----- | ----- | ----- |
  * `-----------------------------------------------' `-----------------------------------------------'
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,   KC_F12, \
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,     KC_NO,     KC_NO,     KC_NO,     KC_NO,    KC_DEL, \
   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_LEFT,   KC_DOWN,   KC_UP,     KC_RGHT,   KC_NO,    KC_NO, \
-  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_MINS,   KC_EQL,    KC_LBRC,   KC_RBRC,   KC_NO,    KC_TRNS, \
+  KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,    KC_MINS,   KC_EQL,    KC_LBRC,   KC_RBRC,   KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,      KC_NO,     KC_NO,     KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS \
 )
 
