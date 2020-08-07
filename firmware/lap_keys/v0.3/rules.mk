228995a3a347ae0ac686b7d0eccc3e418229411f
Justin Frahm <justinmfrahm@gmail.com>
2020-08-07 00:13:08 -0600

    Drop api_sysex and it compiles

--- firmware/lap_keys/v0.3/rules.mk
+++ firmware/lap_keys/v0.3/rules.mk
@@ -28,7 +28,7 @@ AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = yes         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-API_SYSEX_ENABLE = yes
+API_SYSEX_ENABLE = no
 TAP_DANCE_ENABLE = no
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
